478119cd47a1377de28bb6851ec100bece4c4ca8
JeoKim224 <jeokim@hanmail.net>
2015-06-01 14:29:17 +0900

    새프로젝트열기
    부모용 알달이 새로운 프로젝트 열기

---
+++ NewAlcongDalcong_Parents/gen/com/example/newalcongdalcong_parents/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.example.newalcongdalcong_parents;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
