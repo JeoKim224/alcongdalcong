075b256abc521c85e94650898ec4e2f1ff2427f1
JeoKim224 <jeokim@hanmail.net>
2015-06-02 13:05:47 +0900

    Revert "로그인, 가입, 스플래쉬"
    This reverts commit 6d86715e47cf43831b24580db805b1146a3d7c44.

--- AlcongDalcong_P/src/com/example/alcongdalcong_p/MainActivity.java
+++ AlcongDalcong_P/src/com/example/alcongdalcong_p/MainActivity.java
@@ -1,16 +1,14 @@
-package alcongdalcong_p.user;
-
-import com.example.alcongdalcong_p.R;
+package com.example.alcongdalcong_p;
 
 import android.app.Activity;
 import android.os.Bundle;
 
-public class TabMain extends Activity {
+public class MainActivity extends Activity {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_tabmain);
+		setContentView(R.layout.activity_main);
 	}
 
 }
