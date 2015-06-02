ab5d740c315057c23adb50f91d1256f59d6bccb5
JeoKim224 <jeokim@hanmail.net>
2015-06-02 13:24:36 +0900

    로그인, 가입, 스플래쉬
    로그인, 가입, 스플래쉬
    날아갔던거만 다시 살림
    SharedPreference 적용 완료

---
+++ AlcongDalcong_P/src/alcongdalcong_p/user/ConstantVariables.java
@@ -0,0 +1,12 @@
+package alcongdalcong_p.user;
+
+public class ConstantVariables {
+	
+	public static final String SERVER_ADDR = "";
+	public static final String PREF_NAME = "FIRST";
+	
+	public static final String LOGIN = "";
+	public static final String REGISTER = "";
+	
+	
+}
--- AlcongDalcong_P/src/alcongdalcong_p/user/TabMain.java
+++ AlcongDalcong_P/src/alcongdalcong_p/user/TabMain.java
@@ -1,14 +1,16 @@
-package com.example.alcongdalcong_p;
+package alcongdalcong_p.user;
+
+import com.example.alcongdalcong_p.R;
 
 import android.app.Activity;
 import android.os.Bundle;
 
-public class MainActivity extends Activity {
+public class TabMain extends Activity {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
+		setContentView(R.layout.activity_tabmain);
 	}
 
 }
